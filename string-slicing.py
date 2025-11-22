0a13ad59e51222e510d3ff07642b2362331eda7e
Vinayjango101 <vinayoffsec@gmail.com>
2025-11-22 13:36:48 +0530

    initial commit

---
+++ string-slicing.py
@@ -0,0 +1,9 @@
+#Slicing
+#You can return a range of characters by using the slice syntax.
+
+#Specify the start index and the end index, separated by a colon, to return a part of the string.
+
+greet = "Hello World!"
+
+print(greet[2:5])
+
