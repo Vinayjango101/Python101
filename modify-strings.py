bf6e8aa231dd06f183b64789c592ce0f39619cd2
Vinayjango <vinayoffsec@gmail.com>
2025-11-29 13:17:34 +0530

    Added ModifyStrings.py

---
+++ modify-strings.py
@@ -0,0 +1,25 @@
+#Upper Case
+#ExampleGet your own Python Server
+#The upper() method returns the string in upper case:
+
+a = "Hello, World!"
+print(a.upper())
+
+#-----------------------------------------------------------------------------
+
+#Lower Case
+#Example
+#The lower() method returns the string in lower case:
+
+a = "Hello, World!"
+print(a.lower())
+
+#-----------------------------------------------------------------------------
+
+#Remove Whitespace
+#Whitespace is the space before and/or after the actual text, and very often you want to remove this space.
+#Example
+#The strip() method removes any whitespace from the beginning or the end:
+
+a = " Hello, World! "
+print(a.strip()) # returns "Hello, World!"
