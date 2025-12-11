9ceb0e30f0ddbc405208dd191abff32f742bab58
Vinayjango <vinayoffsec@gmail.com>
2025-12-11 13:01:35 +0530

    added string-concatenation.py

---
+++ string-concatenation.py
@@ -0,0 +1,23 @@
+# String Concatenation 
+
+# to concatenate, or combine, two strings we can use the "+" operator 
+
+# For Example 
+# Merge Variable "a" with Variable "b" into Variable "c"
+
+a = "Hello"
+b = "World"
+c = a + b 
+print(c)
+
+# Output Would be "HelloWorld"
+
+# to add a space between them we add a  " "
+
+a = "Hello"
+b = "World"
+
+c = a + " " + b
+print(c)
+
+# Output - "Hello World"
